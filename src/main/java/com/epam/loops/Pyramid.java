ab0f536f31608f45a3a9b3a5817dc8301d76145f
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-18 23:56:43 +0500

    fixed all by my works

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,24 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            String str="";
+            for (int j = 1; j <= cathetusLength; j++) {
+                if (i>=j){
+                    str=j+str;
+                }else {
+                    str=" "+str;
+                }
+            }
+            for (int k = 2; k <= cathetusLength; k++) {
+                if (i>=k){
+                    str=str+k;
+                }else {
+                    str=str+" ";
+                }
+            }
+            System.out.println(str);
+        }
     }
 
     public static void main(String[] args) {
