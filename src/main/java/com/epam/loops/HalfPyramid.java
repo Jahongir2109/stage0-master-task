cfa3f3cac0781d24cd66388b75d36c8f623def2b
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-19 17:28:46 +0500

    fixed all

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,16 +3,28 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
-        for (int i = 0; i < cathetusLength; i++) {
-            String str="";
-            for (int j = 0; j < cathetusLength; j++) {
-                if(cathetusLength-i<=j){
-                    str+="*";
-                }else {
-                    str+=" ";
-                }
+//        for (int i = 0; i < cathetusLength; i++) {
+//            String str="";
+//            for (int j = 0; j < cathetusLength; j++) {
+//                if(cathetusLength-i<=j){
+//                    str+="*";
+//                }else {
+//                    str+=" ";
+//                }
+//            }
+//            System.out.println(str);
+//        }
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" ");
             }
-            System.out.println(str);
+            for (int j = i; j > 0; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
         }
     }
 
